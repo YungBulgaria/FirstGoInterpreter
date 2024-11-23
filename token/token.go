7038067896cdbafb8c41b68aebcd74c355d20018
Mick Enev <menev2@illinois.edu>
2024-11-23 12:30:48 -0600

    Added extended set of tokens to lexer.

--- token/token.go
+++ token/token.go
@@ -16,8 +16,18 @@ const (
 	INT	  = "INT" // 123456
 
 	// Operators
-	ASSIGN	= "="
-	PLUS	= "+"
+	ASSIGN	 = "="
+	PLUS	 = "+"
+	MINUS 	 = "-"
+	BANG	 = "!"
+	ASTERISK = "*"
+	SLASH	 = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ 		= "=="
+	NOT_EQ  = "!="
 
 	// Delimiters
 	COMMA	  = ","
@@ -31,11 +41,21 @@ const (
 	// Keywords 
 	FUNCTION = "FUNCTION"
 	LET 	 = "LET"
+	IF		 = "IF"
+	ELSE 	 = "ELSE"
+	RETURN 	 = "RETURN"
+	TRUE	 = "TRUE"
+	FALSE 	 = "FALSE"
 )
 
 var keywords = map[string]TokenType{
-	"fn":	FUNCTION,
-	"let":	LET,
+	"fn":		FUNCTION,
+	"let":		LET,
+	"if": 		IF,
+	"else": 	ELSE,
+	"true": 	TRUE,
+	"false": 	FALSE,
+	"return": 	RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
