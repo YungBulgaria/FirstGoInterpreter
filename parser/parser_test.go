84cd4c9c720b78752b592eae93182d2a219b64dc
YungBulgaria <mickenev@gmail.com>
2024-11-23 19:14:19 -0600

    Added some parser functionality and started constructing tests.

---
+++ parser/parser_test.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"testing"
+	"donkey/ast"
+	"donkey/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	let x = 5;
+	let y = 10;
+	let foobar = 838383;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statments) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"}
+	}
+}
