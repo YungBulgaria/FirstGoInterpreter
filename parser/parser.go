975f6d96a4c1b1e98c6c3315d6f52e3cc7bec9fb
YungBulgaria <mickenev@gmail.com>
2024-12-01 21:03:53 -0600

    Added some functionality to the parser

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,17 @@ import (
 	"fmt"
 )
 
+const (
+	_ int = iota
+	LOWEST 		
+	EQUALS 		// ==
+	LESSGREATER // > or <
+	SUM 		// +
+	PRODUCT 	// *
+	PREFIX 		// -X or !X
+	CALL 		// myFunction(X)
+)
+
 type Parser struct {
 	l *lexer.Lexer
 	errors []string
@@ -29,11 +40,18 @@ func New(l *lexer.Lexer) *Parser { // Create a new parser object initializing l 
 		errors: []string{},
 	}
 
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
 func (p *Parser) nextToken() { // Sets curToken to peekToken and peekToken to p.l.NextToken()
@@ -62,7 +80,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -103,6 +121,27 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
 
  // Helper functions for executing the parsing above 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
